4d71c5796a207a7e78fc375d2b23f1170e47bee1
SlimDay Ops <atendimento@slimday.com>
2026-04-16 07:39:50 -0300

    fix: adjust script path in index.html and add diagnostic log in main.tsx

--- aplicativo-slimday/src/main.tsx
+++ aplicativo-slimday/src/main.tsx
@@ -2,4 +2,5 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+console.log("SlimDay App: Iniciando montagem do React...");
 createRoot(document.getElementById("root")!).render(<App />);
